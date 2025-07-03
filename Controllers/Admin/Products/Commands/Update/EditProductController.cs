5117a7c959498b86e36292a4d152fffceba6d7c2
Ehsansamiei <ehsan.samiei84@gmail.com>
2025-07-03 15:39:43 +0330

    Edit of Product Controller created and some icons added

--- Controllers/Admin/Products/Commands/Update/EditProductController.cs
+++ Controllers/Admin/Products/Commands/Update/EditProductController.cs
@@ -2,21 +2,25 @@ using DataLayer;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using ParyanPerfume.Dtos;
-
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 
 namespace ParyanPerfume.Controllers.Admin
 {
-    [Route("Admin/Product")]
+    [Route("Admin/Products")]
     public class EditProductController : Controller
     {
         private readonly IProductRepository _productRepository;
         private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly ParyanPerfumeDbContext _paryanPerfumeDbContext;
 
-        public EditProductController(IProductRepository productRepository, IWebHostEnvironment webHostEnvironment)
+        public EditProductController(IProductRepository productRepository,  ParyanPerfumeDbContext paryanPerfumeDbContext, ICategoryRepository categoryRepository, IWebHostEnvironment webHostEnvironment)
         {
             _productRepository = productRepository;
             _webHostEnvironment = webHostEnvironment;
+            _categoryRepository = categoryRepository;
+            _paryanPerfumeDbContext = paryanPerfumeDbContext;
         }
 
         [HttpGet("Edit/{id}")]
@@ -48,7 +52,70 @@ namespace ParyanPerfume.Controllers.Admin
                 ProductSex = product.ProductSex,
                 ShowInSlider = product.ShowInSlider,
             };
+
+            ViewBag.CategoryId = new SelectList(_categoryRepository.GetAllCategories(), "CategoryId", "CategoryName");
             return View(dto);
+        }
+
+        [HttpPost("Edit/{id}")]
+        public async Task<IActionResult> EditProduct(ProductsDto Dto)
+        {
+            if (!ModelState.IsValid)
+            {
+                var errors = ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage).ToList();
+                Console.WriteLine("ModelState Errors: " + string.Join(" | ", errors));
+                return View(Dto);
+            }
+
+            var product = _productRepository.GetProductById(Dto.ProductId);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            product.CategoryId = product.CategoryId;
+            product.ProductId = product.ProductId;
+            product.ProductName = product.ProductName;
+            product.ProductNickName = product.ProductNickName;
+            product.Price = product.Price;
+            product.PricePer100gram = product.PricePer100gram;
+            product.ShortDescription = product.ShortDescription;
+            product.ImageName = product.ImageName;
+            product.ProductBrand = product.ProductBrand;
+            product.ProductDescription = product.ProductDescription;
+            product.ProductSex = product.ProductSex;
+            product.ShowInSlider = product.ShowInSlider;
+
+            if (Dto.ImageFile == null && !string.IsNullOrEmpty(Dto.ImageName)) {
+                product.ImageName = Dto.ImageName;
+            }
+
+            if (Dto.ImageFile != null) {
+                if (!string.IsNullOrEmpty(product.ImageName))
+                {
+                    var oldImagePath = Path.Combine(_webHostEnvironment.WebRootPath, "ProductImages", product.ImageName);
+                    if (System.IO.File.Exists(oldImagePath))
+                    {
+                        System.IO.File.Delete(oldImagePath);
+                    }
+                }
+
+                var fileName = Guid.NewGuid() + Path.GetExtension(Dto.ImageFile.FileName);
+                var filePath = Path.Combine(_webHostEnvironment.WebRootPath, "ProductImages");
+
+                if (!Directory.Exists(filePath))
+                {
+                    Directory.CreateDirectory(filePath);
+                }
+                var newPath = Path.Combine(filePath, fileName);
+                using (var stream = new FileStream(newPath, FileMode.Create))
+                {
+                    await Dto.ImageFile.CopyToAsync(stream);
+                }
+                product.ImageName = fileName;
+            }
+
+            await _productRepository.SaveAsync();
+            return RedirectToAction("GetAllProducts", "GetProdcut");
         }
     }
 }
